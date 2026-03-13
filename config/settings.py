31134ccdf7123b60732d6bc4183088b30cb3d4d1
toshi <d-toshi@i.softbank.jp>
2026-03-14 01:06:42 +0900

    EMAIL_BACKEND修正

--- config/settings.py
+++ config/settings.py
@@ -198,7 +198,7 @@ LOGIN_REDIRECT_URL = '/home/'  # ログイン後の移動先
 ACCOUNT_LOGOUT_REDIRECT_URL = '/home/'  # ログアウト後の移動先
 
 # メールサーバー用の設定（プロ仕様）
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
